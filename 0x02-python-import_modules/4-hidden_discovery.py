e759d3aec70ae79dbf28d2464099de33298f597c
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-11-30 21:06:17 +0100

    100-my_calculator.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4.pyc
+    # Print sorted name from directory
+    for name in sorted(dir(hidden_4.pyc)):
+        # print only names that do not start with __
+        if name[:2] != '__':
+            print("{}".format(name))
+
