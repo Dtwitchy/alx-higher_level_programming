0e641b489232c61f8fb5ea4969b68e801decc20a
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2024-03-28 05:25:38 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     import sys
     import requests
 
-     url = sys.argv[1]
+    url = sys.argv[1]
     response = requests.get(url)
-
+    
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
